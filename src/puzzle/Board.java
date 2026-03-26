4e790902ef2bc23f9bf3889412370e4ec5ba2dc3
AlextricOverload <alexwarren590@gmail.com>
2026-03-26 15:32:25 -0600

    Created Tile and Board constructors

--- src/puzzle/Board.java
+++ src/puzzle/Board.java
@@ -1,5 +1,28 @@
 package puzzle;
 
-class Board {
+public class Board {
+    private final int SIZE;  // Size of the board
+    private Tile[][] tiles;  // Tile array for the board
+    public Board(int size) {
+        SIZE = size;
+
+        // Initialize tiles as a SIZE x SIZE 2D array
+        tiles = new Tile[SIZE][SIZE];
+
+        // Create the solved board
+        // Loop through each row
+        for (int row = 0; row < SIZE; row++) {
+            // Loop through each column
+            for (int col = 0; col < SIZE; col++) {
+                if (col == SIZE-1 && row == SIZE-1) {  // Last tile on the board
+                    tiles[row][col] = null;  // Blank tile
+                }
+                else {  // Tile is not last
+                    tiles[row][col] = new Tile(row, col);  // Regular tile
+                }
+            }
+        }
+        
+    }
     public static void main(String[] args) {}
 } 
--- src/puzzle/Tile.java
+++ src/puzzle/Tile.java
@@ -1,5 +1,11 @@
 package puzzle;
 
-class Tile {
+public class Tile {
+    private final int[] CORRECT_POS;  // Where the tile should go to win the game
+
+    // Constructor
+    public Tile(int row, int col) {
+        CORRECT_POS = new int[]{row, col};
+    }
     public static void main(String[] args) {}
 }
